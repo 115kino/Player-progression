0439ca18f37eead560b2d3ce25263c1f084885b0
Kino <jose33201@hotmail.com>
2017-04-15 21:37:35 -0400

    Added Item/Block handlers

--- src/main/java/com/legacy/player_progression/blocks/BlockHandler.java
+++ src/main/java/com/legacy/player_progression/blocks/BlockHandler.java
@@ -1,5 +1,29 @@
 package com.legacy.player_progression.blocks;
 
-public class BlockHandler {
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class BlockHandler 
+{
+
+	private Block synthesizer;
+
+	public static void initialization()
+	{
+		
+	}
+
+	public static Block register(String unlocalizedName, Block block)
+	{
+		block.setUnlocalizedName(unlocalizedName);
+		block.setRegistryName("player_progression", unlocalizedName);
+
+		GameRegistry.register(block, new ResourceLocation("player_progression", unlocalizedName));
+		GameRegistry.register(new ItemBlock(block).setRegistryName("player_progression", unlocalizedName));
+
+		return block;
+	}
 
 }
--- src/main/java/com/legacy/player_progression/items/ItemHandler.java
+++ src/main/java/com/legacy/player_progression/items/ItemHandler.java
@@ -1,5 +1,27 @@
 package com.legacy.player_progression.items;
 
-public class ItemHandler {
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class ItemHandler 
+{
+
+	public static Item diamond_sphere;
+
+	public static void initialization()
+	{
+		diamond_sphere = register("diamond_sphere", new Item().setMaxStackSize(1));
+	}
+
+	public static Item register(String unlocalizedName, Item item)
+	{
+		item.setUnlocalizedName(unlocalizedName);
+		item.setRegistryName("player_progression", unlocalizedName);
+
+		GameRegistry.register(item, new ResourceLocation("player_progression", unlocalizedName));
+
+		return item;
+	}
 
 }
